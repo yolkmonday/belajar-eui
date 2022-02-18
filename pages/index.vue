02abc6726f978194b7d54769db30f8a6c861a3c9
aripadrian <aripadrian01@gmail.com>
2022-02-18 09:55:47 +0700

    first init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <el-page-header :icon="ArrowLeft" content="detail" />
+  </div>
+</template>
+
+<script>
+import { ArrowLeft } from '@element-plus/icons-vue'
+export default {
+  name: 'IndexPage',
+  components: { ArrowLeft },
+}
+</script>
